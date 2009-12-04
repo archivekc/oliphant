7ffefaaec2965de1859f6dc97c980686c98013ec
Arnaud Betremieux <arnaud.betremieux@keyconsulting.fr>
2009-12-04 12:34:52 +0100

    reorganisation du code

--- src/NotifyListener.java
+++ src/NotifyListener.java
@@ -1,29 +1,48 @@
 class NotifyFlushEventListener extends EventListener
 {
-	private staleIds; // Map de session_id -> Map de UID (string identifiant table + objet) -> derniere version ou null ?
+	private staleUids; // Map de session_id -> Map de UID (string identifiant table + objet) -> true ?
+	private versions; // Map de UID -> derniere version connue
 
 	public Serializable onSessionCreation ?? (SessionCreationEvent event) throws HibernateException {
-		updateStaleUids();
-		// ensuite on supprimme tout (en fait on recupere les notifs arrivees jusqu'a maintenant puis on les enleve pour cette session
-		}
-
-	public Serializable onFlush(FlushEvent event) throws HibernateException {
-		return onPersist(event);
-		}
-
-	public Serializable onPersist(PersistEvent event) throws HibernateException {
-		Object object = event.getObject();
-		if (isStale(object), event.getSession()) {throw new StaleObjectStateException(object.class, id); }
-		return null;
+		updateStaleUidsAndVersions();
+		staleUids.Clear(); // On ne sait pas de quand datent les notifs, donc celles qui ne nous indique pas de numero de version et ont été émises avant le début de cette session ne sont pas intérprétables
 		}
 
 	public Serializable onLoad(PersistEvent event) throws HibernateException {
-		updateStaleUids();
 		// il est peut etre deja stale. Peut on le verifier dans le cas ou on est versionne ? -> est on appele juste apres ou juste avant le load ? Dans ce cas, on regarde si dans staleIds on a une version et si c'est le cas on la compare a notre version. Si pas dans dirtyid on est clean. Si version correcte on retire de dirtyIds de cette session. Si version ancienne, on staleobjectstateexception, et on garde le dirtyid.
 		// Cas pas de versionnage : pn enregistre que l'objet est clean pour cette session donc on le retire des dirtyIds de cette session
+		return processEvent(event);
 		}
 
-	public boolean isStale(Object object, Session session) {
+	public Serializable onPersist(PersistEvent event) throws HibernateException {
+		return processEvent(event);
+		}
+
+	public Serializable onFlush(FlushEvent event) throws HibernateException {
+		return processEvent(event);
+		}
+
+	public Serializable onCommit(CommitEvent event) throws HibernateException {
+		return processEvent(event);
+		}
+
+	public Serializable processEvent(PersistEvent event) throws HibernateException {
+		updateStaleUidsAndVersions();
+		Object object = event.getObject();
+		if (isKnownToBeStale(object), event.getSession()) {
+			if (isKnownToBeStaleInL2(object)) { session.getSessionFactory().evict(object); } // l'objet est stale en L2, on le retire du cache L2
+			throw new StaleObjectStateException(object.class, id); // pas toujours pour un load, configurable
+			}
+		return null;
+		}
+
+	public boolean isKnownToBeStaleInL2(Object object) {
+		Versioning.getVersion(Object[] fields, EntityPersister persister) // Extract the optimisitc locking value out of the entity state snapshot.
+		// comparer version L2 et derniere version connue
+		// retourner toujours false pour les non versionnes
+		}
+
+	public boolean isKnownToBeStale(Object object, Session session) {
 		if (in staleIds uid(object)) {return true;}
 		updateStaleUids();
 		if (in staleIds uid(object)) {return true;}
