4240985965c8b4d1a92ded9ce68d5cbef5a8f8b7
Arnaud Betremieux <arnaud.betremieux@keyconsulting.fr>
2009-12-23 10:49:52 +0100

    Regroup configuration in utils, add PersistentVersionedObject to mappings

--- src/Utils.java
+++ src/Utils.java
@@ -5,75 +5,66 @@ import java.sql.SQLException;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.AnnotationConfiguration;
 
-public class Utils {
-	public static Connection getJDBCConnection() throws SQLException {
+public class Utils
+	{
+	public static Connection getJDBCConnection() throws SQLException
+		{
 		String url = "jdbc:postgresql://localhost/hibernate?user=hibernate&password=hibernate333";
 		return DriverManager.getConnection(url);
-	}
+		}
 	
 	private static final SessionFactory sessionFactory;
 	private static final SessionFactory magicSessionFactory;
-	static {
-		try {
+	static
+		{
+		try
+			{
 			AnnotationConfiguration config = new AnnotationConfiguration();
-			config.setProperty("hibernate.dialect",
-					"org.hibernate.dialect.PostgreSQLDialect");
-			config.setProperty("hibernate.connection.driver_class",
-					"org.postgresql.Driver");
-			config.setProperty("hibernate.connection.url",
-					"jdbc:postgresql://localhost/hibernate");
-			config.setProperty("hibernate.connection.username", "hibernate");
-			config.setProperty("hibernate.connection.password", "hibernate333");
-			config.setProperty("hibernate.connection.pool_size", "1");
-			//config.setProperty("hibernate.connection.autocommit", "true");
-			config.setProperty("hibernate.cache.provider_class", "org.hibernate.cache.NoCacheProvider");
-			config.setProperty("hibernate.hbm2ddl.auto", "create-drop");
-			config.setProperty("hibernate.show_sql", "true");
-			config.setProperty("hibernate.transaction.factory_class", "org.hibernate.transaction.JDBCTransactionFactory");
-			config.setProperty("hibernate.current_session_context_class", "thread");
-
-			config.setProperty("hibernate.select_before_update", "true");
-
-			config.addAnnotatedClass(PersistentObject.class);
+			fillConfig(config);
 			sessionFactory = config.buildSessionFactory();
-		} catch (Throwable ex) {
+			AnnotationConfiguration magicConfig = new MagicAnnotationConfiguration();
+			fillConfig(magicConfig);
+			magicSessionFactory = magicConfig.buildSessionFactory();
+			}
+		catch (Throwable ex)
+			{
 			System.err.println("Initial SessionFactory creation failed." + ex);
 			throw new ExceptionInInitializerError(ex);
+			}
 		}
-		try {
-			AnnotationConfiguration config = new MagicAnnotationConfiguration();
-			config.setProperty("hibernate.dialect",
-					"org.hibernate.dialect.PostgreSQLDialect");
-			config.setProperty("hibernate.connection.driver_class",
-					"org.postgresql.Driver");
-			config.setProperty("hibernate.connection.url",
-					"jdbc:postgresql://localhost/hibernate");
-			config.setProperty("hibernate.connection.username", "hibernate");
-			config.setProperty("hibernate.connection.password", "hibernate333");
-			config.setProperty("hibernate.connection.pool_size", "1");
-			//config.setProperty("hibernate.connection.autocommit", "true");
-			config.setProperty("hibernate.cache.provider_class", "org.hibernate.cache.NoCacheProvider");
-			config.setProperty("hibernate.hbm2ddl.auto", "create-drop");
-			config.setProperty("hibernate.show_sql", "true");
-			config.setProperty("hibernate.transaction.factory_class", "org.hibernate.transaction.JDBCTransactionFactory");
-			config.setProperty("hibernate.current_session_context_class", "thread");
 
-			config.setProperty("hibernate.select_before_update", "true");
-
-			config.addAnnotatedClass(PersistentObject.class);
-			magicSessionFactory = config.buildSessionFactory();
-		} catch (Throwable ex) {
-			System.err.println("Initial SessionFactory creation failed." + ex);
-			throw new ExceptionInInitializerError(ex);
-		}
-	}
-
-	public static SessionFactory getSessionFactory() {
+	public static SessionFactory getSessionFactory()
+		{
 		return sessionFactory;
-	}
+		}
 
-	public static SessionFactory getMagicSessionFactory() {
+	public static SessionFactory getMagicSessionFactory()
+		{
 		// TODO Auto-generated method stub
 		return magicSessionFactory;
+		}
+	
+	private static void fillConfig(AnnotationConfiguration config)
+		{
+		config.setProperty("hibernate.dialect",
+		"org.hibernate.dialect.PostgreSQLDialect");
+		config.setProperty("hibernate.connection.driver_class",
+				"org.postgresql.Driver");
+		config.setProperty("hibernate.connection.url",
+				"jdbc:postgresql://localhost/hibernate");
+		config.setProperty("hibernate.connection.username", "hibernate");
+		config.setProperty("hibernate.connection.password", "hibernate333");
+		config.setProperty("hibernate.connection.pool_size", "1");
+		//config.setProperty("hibernate.connection.autocommit", "true");
+		config.setProperty("hibernate.cache.provider_class", "org.hibernate.cache.NoCacheProvider");
+		config.setProperty("hibernate.hbm2ddl.auto", "create-drop");
+		config.setProperty("hibernate.show_sql", "true");
+		config.setProperty("hibernate.transaction.factory_class", "org.hibernate.transaction.JDBCTransactionFactory");
+		config.setProperty("hibernate.current_session_context_class", "thread");
+		
+		config.setProperty("hibernate.select_before_update", "true");
+		
+		config.addAnnotatedClass(PersistentObject.class);
+		config.addAnnotatedClass(PersistentVersionedObject.class);
+		}
 	}
-}
