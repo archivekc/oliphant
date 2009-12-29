766efaf0bf41b298203030c6c67e539f042e2def
Arnaud Betremieux <arnaud.betremieux@keyconsulting.fr>
2009-12-29 15:46:49 +0100

    Moved java code to package fr.keyconsulting.oss.oliphant

--- src/SpecificNotifyListener.java
+++
@@ -1,9 +0,0 @@
-
-import java.util.List;
-
-public interface SpecificNotifyListener
-	{
-	public void setUp(); // Setup the notification system (create triggers, subscribe to update notifications, etc ?)
-	public List<Notification> getLatestUpdates(); // Return the latest notifications
-	public void tearDown(); // Close the system properly (remove triggers, unsubscribe, etc ?)
-	}
