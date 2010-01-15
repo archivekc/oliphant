073613fb96a51f05533eeacb95bf399a68a3bf53
Arnaud Betremieux <arnaud.betremieux@keyconsulting.fr>
2010-01-15 16:14:14 +0100

    Code cleanup : string+= -> stringbuilder

--- src/fr/keyconsulting/oliphant/PostgreSQLListenerAuxiliary.java
+++ src/fr/keyconsulting/oliphant/PostgreSQLListenerAuxiliary.java
@@ -19,45 +19,49 @@ public class PostgreSQLListenerAuxiliary extends AbstractAuxiliaryDatabaseObject
 
 	public String sqlCreateString(Dialect dialect, Mapping mapping, String defaultCatalog, String defaultSchema)
 		{
-		String sql = "";
+		StringBuilder sb = new StringBuilder();
+
 		for(Iterator i = config.getTableMappings(); i.hasNext();)
 			{
 			Table t = (Table) i.next();
 			String table = t.getName().toLowerCase();
 			
-			sql += "CREATE OR REPLACE FUNCTION oliphant_"+table+"() RETURNS TRIGGER AS $$\n";
-			sql += "	DECLARE\n";
-			sql += "		VERSION TEXT;\n";
-			sql += "	BEGIN\n";
-			sql += "		IF TG_OP = 'UPDATE' THEN\n";
-			sql += "			VERSION := NEW.VERSION;\n";
-			sql += "		ELSIF TG_OP = 'DELETE' THEN\n";
-			sql += "			VERSION := -1;\n";
-			sql += "		END IF;\n";
-			sql += "		PERFORM send_notify('oliphant', '"+table+"#' || OLD.ID || '###' || VERSION); RETURN NULL;\n"; 
-			sql += "	END;\n";
-			sql += "$$ LANGUAGE 'plpgsql';\n";
-			sql += "\n";
-			sql += "CREATE TRIGGER oliphant_"+table+"_trg\n";
-			sql += "	AFTER DELETE OR UPDATE ON "+table+"\n";
-			sql += "	FOR EACH ROW EXECUTE PROCEDURE oliphant_"+table+"();\n";
-			sql += "\n";
+			sb.append("CREATE OR REPLACE FUNCTION oliphant_"+table+"() RETURNS TRIGGER AS $$\n");
+			sb.append("	DECLARE\n");
+			sb.append("		VERSION TEXT;\n");
+			sb.append("	BEGIN\n");
+			sb.append("		IF TG_OP = 'UPDATE' THEN\n");
+			sb.append("			VERSION := NEW.VERSION;\n");
+			sb.append("		ELSIF TG_OP = 'DELETE' THEN\n");
+			sb.append("			VERSION := -1;\n");
+			sb.append("		END IF;\n");
+			sb.append("		PERFORM send_notify('oliphant', '"+table+"#' || OLD.ID || '###' || VERSION); RETURN NULL;\n");
+			sb.append("	END;\n");
+			sb.append("$$ LANGUAGE 'plpgsql';\n");
+			sb.append("\n");
+			sb.append("CREATE TRIGGER oliphant_"+table+"_trg\n");
+			sb.append("	AFTER DELETE OR UPDATE ON "+table+"\n");
+			sb.append("	FOR EACH ROW EXECUTE PROCEDURE oliphant_"+table+"();\n");
+			sb.append("\n");
 			}
-		return sql;
+
+		return sb.toString();
 		}
 
 	public String sqlDropString(Dialect dialect, String defaultCatalog, String defaultSchema)
 		{
-		String sql = "";
+		StringBuilder sb = new StringBuilder();
+
 		for(Iterator i = config.getTableMappings(); i.hasNext();)
 			{
 			Table t = (Table) i.next();
 			String table = t.getName().toLowerCase();
 			
-			sql += "DROP FUNCTION oliphant_"+table+"()\n";
-			sql += "\n";
+			sb.append("DROP FUNCTION oliphant_"+table+"()\n");
+			sb.append("\n");
 			}
-		return sql;
+
+		return sb.toString();
 		}
 
 	}
